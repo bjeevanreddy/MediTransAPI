a274956429c8475799bf207e501271aeaeec7d62
jeevanreddyb <41283608+jeevanreddyb@users.noreply.github.com>
2020-03-01 15:31:35 +0530

    update

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use('/api', medicorouter);
 //app.use('/jobs', jobsrouter);
 
 
-mongoose.connect('mongodb+srv://jeevan:<password>@jeevan-9ruiz.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (error, res) => {
+mongoose.connect('mongodb+srv://jeevan:jeevan>@jeevan-9ruiz.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (error, res) => {
     if (res) {
         console.log('Connected successfully');
     }
