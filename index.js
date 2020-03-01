4123f0eba8303f0d829052580d9e96cce71022da
jeevanreddyb <41283608+jeevanreddyb@users.noreply.github.com>
2020-03-01 19:08:16 +0530

    ec

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ var app = express();
 var bodyParser = require('body-parser');
 const PORT=process.env.PORT||3000;
 var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+ app.use(express.static(distDir));
 const mongoose = require('mongoose');
 var app = express();
-//const defaultcntrl=require('./routers/default.router');
 const defaultcntrl=require('./controllers/default.cntrl');
 const medicorouter = require('./routers/medicoReg.router');
-
 app.use(bodyParser.json());
 app.get('/',defaultcntrl.defaultCheck);
 app.use('/api', medicorouter);
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MediTrans', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (error, res) => {
+
+mongoose.connect('mongodb://jeevan:jeevan@jeevan-shard-00-00-9ruiz.mongodb.net:27017,jeevan-shard-00-01-9ruiz.mongodb.net:27017,jeevan-shard-00-02-9ruiz.mongodb.net:27017/Meditrans?ssl=true&replicaSet=jeevan-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (error, res) => {
     if (res) {
         console.log('Connected successfully');
     }
@@ -21,10 +20,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MediTrans
         console.log("Something error occured");
     }
 });
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
-
 // mongoose.connect('mongodb://localhost:27017/MediTrans', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (error, res) => {
 //     if (res) {
 //         console.log('DB1 Connected successfully');
@@ -34,5 +29,5 @@ if (process.env.NODE_ENV === 'production') {
 //     }
 // });
 app.listen(PORT, function () {
-    console.log(`Server is starting at ${PORT}`);
+    console.log('Server running');
 })
