b438ac6ee81421b000482a681b6168cdac7e8cdc
jeevanreddyb <41283608+jeevanreddyb@users.noreply.github.com>
2020-03-01 18:06:19 +0530

    def

--- routers/default.router.js
+++ routers/default.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const defaultcntrl=require('../controllers/default.cntrl');
-router.get('/',defaultcntrl.defaultCheck);
+
+
 module.exports=router;
