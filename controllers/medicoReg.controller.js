6aad458cb7bb1376f65650a554f5956f1529091b
jeevanreddyb <41283608+jeevanreddyb@users.noreply.github.com>
2020-03-01 21:16:17 +0530

    cc

--- controllers/medicoReg.controller.js
+++ controllers/medicoReg.controller.js
@@ -11,30 +11,35 @@ const medicocntrl={
             console.log(medicopresent);
             if(medicopresent.length>=1)
             {
-                res.send("You are already registered").status(200);
+                res.send("You are already registered");
+                res.status(200);
             }
             else{
                 let register=await medicosvc.addMedico(req.body);
                 if(register)
                 {
-                    res.send("Registered Successfully!!!").status(200);
+                    res.send("Registered Successfully!!!");
+                    res.status(200);
                 }  
             }
         }
         catch(error)
         {
             res.send(error);
+            res.status(500);
         }
     },
     getallmedicos: async function(req,res)
     {
         try{
             let allmedicos=await medicosvc.getmedicos();
-            res.json(allmedicos).status(200);
+            res.json(allmedicos);
+            res.status(200);
         }
         catch(error)
         {
-            res.send(error).status(500);
+            res.send(error);
+            res.status(500);
         }
     }
 }
